3e02220fcbb796e3b8230fadff5bb70508d9cd72
Foufou.exe <thibaut.maurras34@gmail.com>
2025-02-23 15:27:20 +0100

    🐞 fix(SEED): UPDATE SQL
    MODIF SQL

--- database/seed_fidegourmet.sql
+++ database/seed_fidegourmet.sql
@@ -1,13 +1,11 @@
 CREATE TABLE
-  `Dim_Date` (`Date_ID` INT PRIMARY KEY, `Date` DATE NOT NULL);
-
-CREATE TABLE
   `Dim_Client` (
     `Client_ID` INT PRIMARY KEY,
     `Email` VARCHAR(100),
     `Last_Name` VARCHAR(100),
     `First_Name` VARCHAR(100),
-    `Phone` VARCHAR(50)
+    `Phone` VARCHAR(50),
+    `Address` VARCHAR(255)
   );
 
 CREATE TABLE
@@ -16,12 +14,6 @@ CREATE TABLE
     `Name` VARCHAR(255) NOT NULL,
     `Category` VARCHAR(255),
     `Price` DECIMAL(10, 2) NOT NULL
-  );
-
-CREATE TABLE
-  `Dim_Channel` (
-    `Channel_ID` INT PRIMARY KEY,
-    `Type` VARCHAR(50) NOT NULL
   );
 
 CREATE TABLE
@@ -36,19 +28,16 @@ CREATE TABLE
     `Sale_ID` VARCHAR(50) PRIMARY KEY,
     `Quantity` INT NOT NULL,
     `Price` DECIMAL(10, 2) NOT NULL,
+    `Type` VARCHAR(50) NOT NULL,
+    `Date` DATE NOT NULL,
     `FK_Date_ID` INT NOT NULL,
     `FK_Client_ID` INT,
     `FK_Product_ID` VARCHAR(50) NOT NULL,
-    `FK_Channel_ID` INT NOT NULL,
-    `FK_Store_ID` VARCHAR(50) NOT NULL
+    `FK_Store_ID` VARCHAR(50)
   );
-
-ALTER TABLE `Fact_Sales` ADD FOREIGN KEY (`FK_Date_ID`) REFERENCES `Dim_Date` (`Date_ID`);
 
 ALTER TABLE `Fact_Sales` ADD FOREIGN KEY (`FK_Client_ID`) REFERENCES `Dim_Client` (`Client_ID`);
 
 ALTER TABLE `Fact_Sales` ADD FOREIGN KEY (`FK_Product_ID`) REFERENCES `Dim_Product` (`Product_ID`);
-
-ALTER TABLE `Fact_Sales` ADD FOREIGN KEY (`FK_Channel_ID`) REFERENCES `Dim_Channel` (`Channel_ID`);
 
 ALTER TABLE `Fact_Sales` ADD FOREIGN KEY (`FK_Store_ID`) REFERENCES `Dim_Store` (`Store_ID`);
